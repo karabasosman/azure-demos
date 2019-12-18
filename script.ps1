5067b4336c5dd6bbafe6914817a1ae113937f424
Osman Seckin Karabas <karabasosman@hotmail.com>
2019-12-18 05:21:22 +0300

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,88 +1,76 @@
-
-#Login-AzureRmAccount
-
-
 Write-Host "....Azure Script Started....."
 
 $vmSize="Standard_DS1_V2"
-$start=1;  #Baslangic kullanici numarasi
-$end=3  #Bitis kullanici numarasi
-$prefix="zxcwc"
-$location="northeurope"
-$networksCidr="192.25.0.0/24"
+$start=56;  #Baslangic kullanici numarasi
+$end=57; #Bitis kullanici numarasi
+$prefix="doxts"
+$location="westeurope"
+$networkResourceGroupName=$prefix+"network-rg"
+$networksCidr="172.0.0.0/16"
+$vmSubnetCidr="172.0.0.0/24"
+$bastionSubnetCidr="172.0.1.0/24"
+$bastionHostname=$prefix+"-bastion"
 $shutdownTime="19:00"
 $adminPassword="qweASD123!!!"
 $securePassword = $adminPassword | ConvertTo-SecureString -AsPlainText -Force
 
-
 Write-Host "....Azure Network Setup....."
 
+$commonRg=New-AzureRmResourceGroup -Name $networkResourceGroupName -Location $location -Force
 
-$commonRg=New-AzureRmResourceGroup -Name $prefix-common-rg -Location $location -Force
+$vmSubnet=New-AzureRmVirtualNetworkSubnetConfig -Name 'default' -AddressPrefix $vmSubnetCidr
 
-$vmSubnet=New-AzureRmVirtualNetworkSubnetConfig -Name $prefix-vm-subnet -AddressPrefix $networksCidr
 
-$vnet=New-AzureRmVirtualNetwork -Name $prefix-vnet -ResourceGroupName $prefix-common-rg -Location $location -AddressPrefix $networksCidr -Subnet $vmSubnet -Force
+$vnet=New-AzureRmVirtualNetwork -Name $prefix-vnet -ResourceGroupName $networkResourceGroupName -Location $location -AddressPrefix $networksCidr -Subnet $vmSubnet -Force
+
+Write-Host "....Azure Bastion Setup....."
+
+$dep=New-AzureRmResourceGroupDeployment -ResourceGroupName $commonRg.ResourceGroupName `
+    -Name $prefix-bastion-deployment `
+    -TemplateFile https://raw.githubusercontent.com/karabasosman/azure-demos/dotnet-academy/bastion.json `
+    -location $location `
+    -bastion-host-name $bastionHostname `
+    -bastion-subnet-ip-prefix $bastionSubnetCidr `
+    -vnet-name $prefix-vnet
 
 Write-Host "....Azure VM Setup....."
 
 $scriptBlock = {
-  param ($rg,$prefix,$i,$location,$securePassword,$vnet,$vmSize,$diagStorageName,$shutdownTime)
-  $dep=New-AzureRmResourceGroupDeployment -ResourceGroupName $rg.ResourceGroupName `
-  -Name $prefix-$i-deployment `
-  -TemplateFile https://raw.githubusercontent.com/karabasosman/azure-demos/master/template.json `
-  -TemplateParameterFile https://raw.githubusercontent.com/karabasosman/azure-demos/master/parameters.json `
-  -location $location `
-  -adminPassword $securePassword `
-  -adminUsername $prefix `
-  -virtualNetworkId $vnet.Id `
-  -subnetName $prefix-vm-subnet `
-  -networkInterfaceName $prefix-vm-$i-nic `
-  -networkSecurityGroupName $prefix-vm-$i-nsg `
-  -publicIpAddressName $prefix-$i-pip `
-  -virtualMachineName $prefix-vm-$i `
-  -virtualMachineSize $vmSize `
-  -diagnosticsStorageAccountName $diagStorageName  `
-  -autoShutdownTime $shutdownTime `
-  -publicIpAddressType Dynamic `
-  -publicIpAddressSku Basic `
-  -osDiskType Premium_LRS `
-  -diagnosticsStorageAccountType Standard_LRS `
-  -diagnosticsStorageAccountKind Storage `
-  -autoShutdownTimeZone UTC `
-  -autoShutdownStatus Enabled `
-  -autoShutdownNotificationStatus Disabled `
-  -autoShutdownNotificationLocale en
+    param ($rg,$prefix,$i,$location,$securePassword,$vnet,$vmSize,$diagStorageName,$shutdownTime)
+
+    $dep=New-AzureRmResourceGroupDeployment -ResourceGroupName $rg.ResourceGroupName `
+    -Name $prefix-$i-deployment `
+    -TemplateFile https://raw.githubusercontent.com/karabasosman/azure-demos/dotnet-academy/template.json `
+    -location $location `
+    -vm_size $vmSize `
+    -website_name $prefix-$i-app `
+    -sql_server_name $prefix-$i-sql-srv `
+    -vm_name $prefix-$i `
+    -serverfarms_name $prefix-$i-asp `
+    -diag_str_name $diagStorageName `
+    -nic_name $prefix-$i-nic `
+    -vnet_id $vnet.Id `
+    -ip_name $prefix-$i-pip `
+    -nsg_name $prefix-$i-nsg `
+    -shutdown_time $shutdownTime `
+    -admin_password $securePassword `
+    -admin_username $prefix-adm
 }
 
 For ($i=$start; $i -le $end; $i++) {
-    
 
-
-   $rg=New-AzureRmResourceGroup -Name $prefix-user-$i-rg -Location $location -Force
+    $rg=New-AzureRmResourceGroup -Name $prefix-user-$i-rg -Location $location -Force
 
     Write-Host "....Resource group $prefix-user-$i created...."
 
     $diagStorageName=$prefix+"user"+$i+"diagsa"
 
-    $jobName=$prefix+"user"+$i
-  
-    Start-Job -ScriptBlock $scriptBlock -ArgumentList @($rg,$prefix,$i,$location,$securePassword,$vnet,$vmSize,$diagStorageName,$shutdownTime)
+    $job=Start-Job -ScriptBlock $scriptBlock -ArgumentList @($rg,$prefix,$i,$location,$securePassword,$vnet,$vmSize,$diagStorageName,$shutdownTime)
 
+    $job.Id
 
-
-    $workspaceName=$prefix+"ml"+$i
-
-    $mlDep=New-AzureRmResourceGroupDeployment -ResourceGroupName $rg.ResourceGroupName `
-                                                -Name $prefix-$i-deployment `
-                                                -workspaceName $workspaceName `
-                                                  -location $location `
-                                               -TemplateFile https://raw.githubusercontent.com/karabasosman/azure-demos/master/azure.ml.json 
-                                             
-                                                                                      
-
-     Write-Host "....VM $prefix-user-$i created...."
-   
-}
+    Write-Host "....Job $job.Id created...."
+    Write-Host "Note : If you want to check job status , use command Get-Job -Id  "
+}    
 
 Write-Host "....Azure Script Completed....."
